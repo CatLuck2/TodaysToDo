61d14426435a6d034e3d8c63a4dd5efc782ec312
猫吉 <freeaddress834@gmail.com>
2020-10-14 11:37:32 +0900

    MainViewController - Viewを紐付け、枠線と角丸の設定をViewに移行

--- TodaysToDo/Controller/MainViewController.swift
+++ TodaysToDo/Controller/MainViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var todoListView: UIStackView!
+    @IBOutlet weak var parentViewOfStack: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        todoListView.layer.borderWidth = 0.5
-        todoListView.layer.cornerRadius = 15
+        parentViewOfStack.layer.borderWidth = 0.5
+        parentViewOfStack.layer.cornerRadius = 15
     }
 
     @IBAction func todoListViewTapGesture(_ sender: UITapGestureRecognizer) {
