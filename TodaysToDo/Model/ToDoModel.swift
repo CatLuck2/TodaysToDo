b7fca3913c044206549029ada48d234d1f7f32fd
猫吉 <freeaddress834@gmail.com>
2020-10-15 08:09:43 +0900

    Realmで使用するモデルの、フォルダ/ファイル/クラス、を追加
    ・フォルダ：Model
    ・ファイル名：ToDoModel
    ・モデル名：ToDoList([String])

---
+++ TodaysToDo/Model/ToDoModel.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoModel.swift
+//  TodaysToDo
+//
+//  Created by Nekokichi on 2020/10/15.
+//
+
+import UIKit
+
+class ToDoModel: NSObject {
+    @objc dynamic var toDoList: [String]!
+}
