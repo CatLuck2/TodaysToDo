2f4bea40122468cd4584f2cc1aecfc899a22717e
猫吉 <freeaddress834@gmail.com>
2020-10-15 17:41:13 +0900

    ToDoListEditViewController - tableViewを紐付け、tableView関連のメソッドを用意

--- TodaysToDo/Controller/ToDoListEditViewController.swift
+++ TodaysToDo/Controller/ToDoListEditViewController.swift
@@ -7,12 +7,27 @@
 
 import UIKit
 
-class ToDoListEditViewController: UIViewController {
+class ToDoListEditViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var todoListTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "")
+        return cell!
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
     }
 
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
