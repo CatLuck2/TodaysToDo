81565786b961be3aeee222f002c95b7546482f19
猫吉 <freeaddress834@gmail.com>
2020-10-18 11:07:10 +0900

    タスク追加画面に2つのBarButtonを設置、紐付け
    ・2つのBarButton - Add,Cancel

--- TodaysToDo/Controller/Main/ToDoListAddViewController.swift
+++ TodaysToDo/Controller/Main/ToDoListAddViewController.swift
@@ -14,16 +14,11 @@ class ToDoListAddViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addTodoItemButton(_ sender: UIBarButtonItem) {
     }
-    */
-
+    
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+    }
+    
 }
