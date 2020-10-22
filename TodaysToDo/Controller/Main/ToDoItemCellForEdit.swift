030c7ecf118e7e61eee3f8212352f3fcddbe86be
猫吉 <freeaddress834@gmail.com>
2020-10-22 23:44:00 +0900

    ToDoItemCelLForEdit.swift/.xibを追加、textFieldを設置

---
+++ TodaysToDo/Controller/Main/ToDoItemCellForEdit.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoItemCellForEdit.swift
+//  TodaysToDo
+//
+//  Created by Nekokichi on 2020/10/22.
+//
+
+import UIKit
+
+class ToDoItemCellForEdit: UITableViewCell {
+
+    @IBOutlet weak var todoItemTextField: UITextField!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
