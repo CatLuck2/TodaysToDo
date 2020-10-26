1fe1d7ca1b5aea5639e885136e1018bc591ef6af
猫吉 <freeaddress834@gmail.com>
2020-10-26 23:30:51 +0900

    Main.storyboard > ポップアップ用のViewControllerを作成
    ・クラス：PopupViewController.swift
    ・メイン画面とSegueで接続
    ・セグエID：segueForPopup
    ・TransitionStyle: CrossDisolve
    ・Presentation: OverCurrentContext
    ・背景色を黒、透過度を20%

---
+++ TodaysToDo/Controller/Main/PopupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PopupViewController.swift
+//  TodaysToDo
+//
+//  Created by Nekokichi on 2020/10/26.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    @IBAction func closeButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
