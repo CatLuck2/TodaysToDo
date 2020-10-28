b88522a0ed10e845336c6d701d29f0bb55051201
猫吉 <freeaddress834@gmail.com>
2020-10-28 23:26:55 +0900

    MainViewController > タスクリストがない場合のtodoListStackVIewをレイアウトする処理を追加

--- TodaysToDo/Controller/Main/MainViewController.swift
+++ TodaysToDo/Controller/Main/MainViewController.swift
@@ -19,20 +19,48 @@ class MainViewController: UIViewController {
         // Realmにデータが保存されてるかを確認
         let realm = try! Realm()
         if RealmResults.sharedInstance[0].todoList.isEmpty == false {
-            todoListStackView.layer.borderWidth = 1
             RealmResults.sharedInstance = realm.objects(ToDoModel.self)
-            setTodoList(numberOfItems: RealmResults.sharedInstance[0].todoList.count)
+            setTodoListForEdit(numberOfItems: RealmResults.sharedInstance[0].todoList.count)
+        } else {
+            setTodoListForAdd()
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        todoListStackView.layer.cornerRadius = 5
         todoListStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(setTapGestureInTodoListView(_:))))
     }
 
     // タスクリストのレイアウトを調整
-    private func setTodoList(numberOfItems: Int) {
+    private func setTodoListForAdd() {
+        todoListStackView.layer.borderWidth = 0
+        todoListStackView.layer.cornerRadius = 5
+        // todoListStackViewの子要素を全て削除
+        let subviews = todoListStackView.subviews
+        for subview in subviews {
+            subview.removeFromSuperview()
+        }
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 59).isActive = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        let label = UILabel()
+        label.text = "タスクを追加"
+        label.textAlignment = .center
+        view.addSubview(label)
+
+        // AutoLayout
+        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
+        label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8).isActive = true
+        view.rightAnchor.constraint(equalTo: label.rightAnchor, constant: 8).isActive = true
+
+        todoListStackView.addArrangedSubview(view)
+    }
+
+    private func setTodoListForEdit(numberOfItems: Int) {
+        todoListStackView.layer.borderWidth = 1
         // todoListStackViewの子要素を全て削除
         let subviews = todoListStackView.subviews
         for subview in subviews {
