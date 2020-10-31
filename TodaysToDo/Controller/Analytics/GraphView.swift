337d4e321d3e68c6356176d0ff53f71531be4c3a
猫吉 <freeaddress834@gmail.com>
2020-10-31 17:16:11 +0900

    グラフ用のカスタムViewを作成

---
+++ TodaysToDo/Controller/Analytics/GraphView.swift
@@ -0,0 +1,20 @@
+//
+//  GraphView.swift
+//  TodaysToDo
+//
+//  Created by Nekokichi on 2020/10/31.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
