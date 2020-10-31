2075156813fc3e47fe0e9ea64fdf6c26dff05fc1
猫吉 <freeaddress834@gmail.com>
2020-10-31 17:20:17 +0900

    AnalyticsViewController > UI部品を紐付け

--- TodaysToDo/Controller/Analytics/AnalyticsViewController.swift
+++ TodaysToDo/Controller/Analytics/AnalyticsViewController.swift
@@ -8,4 +8,14 @@
 import UIKit
 
 class AnalyticsViewController: UIViewController {
+
+    @IBOutlet weak var graphSegment: UISegmentedControl!
+    @IBOutlet weak var graphView: UIView!
+    @IBOutlet weak var totalCompletedTaskLabel: UILabel!
+    @IBOutlet weak var rateCompletedTaskLabel: UILabel!
+
+    @IBAction func graphSegment(_ sender: UISegmentedControl) {
+    }
+
+
 }
