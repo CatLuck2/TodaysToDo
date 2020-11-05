5730b20f8d2ae69cb64ca1df5c8d16450421f18d
猫吉 <freeaddress834@gmail.com>
2020-11-05 17:24:57 +0900

    SettingsViewController > セクション/メニュー表示/分岐をenumで実装

--- TodaysToDo/Controller/Settings/SettingsViewController.swift
+++ TodaysToDo/Controller/Settings/SettingsViewController.swift
@@ -7,6 +7,29 @@
 
 import UIKit
 
+// セクション用
+private enum SectionType: Int {
+    case general //一般
+    case notification //通知・アラート
+    case other //その他
+}
+// 一般メニュ-用
+private enum GeneralType: Int {
+    case news //お知らせ
+}
+// 通知・アラートメニュー用
+private enum NotificationType: Int {
+    case sound //サウンド
+    case badge //バッジ
+}
+// その他メニュー用
+private enum OtherType: Int {
+    case help //ヘルプ
+    case share //シェア
+    case developerAccount //開発者Twitter
+    case contact //お問い合わせ
+}
+
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet private weak var settingsTableView: UITableView!
@@ -29,7 +52,17 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        settingsSectionTitle[section].count
+        guard let sectionType = SectionType(rawValue: section) else {
+            return 0
+        }
+        switch sectionType {
+        case .general:
+            return settingsMenuTitle[0].count
+        case .notification:
+            return settingsMenuTitle[1].count
+        case .other:
+            return settingsMenuTitle[2].count
+        }
     }
 
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -38,12 +71,58 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellForSettingsID", for: indexPath)
-        cell.textLabel?.text = settingsMenuTitle[indexPath.section][indexPath.row]
+        guard let sectionType = SectionType(rawValue: indexPath.section) else {
+            return cell
+        }
+        switch sectionType {
+        case .general:
+            cell.textLabel?.text = settingsMenuTitle[0][indexPath.row]
+        case .notification:
+            cell.textLabel?.text = settingsMenuTitle[1][indexPath.row]
+        case .other:
+            cell.textLabel?.text = settingsMenuTitle[2][indexPath.row]
+        }
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        guard let sectionType = SectionType(rawValue: indexPath.section) else {
+            return
+        }
+        switch sectionType {
+        case .general:
+            guard let generalType = GeneralType(rawValue: indexPath.row) else {
+                return
+            }
+            switch generalType {
+            case .news:
+                break
+            }
+        case .notification:
+            guard let notificationType = NotificationType(rawValue: indexPath.row) else {
+                return
+            }
+            switch notificationType {
+            case .sound:
+                break
+            case .badge:
+                break
+            }
+        case .other:
+            guard let otherType = OtherType(rawValue: indexPath.row) else {
+                return
+            }
+            switch otherType {
+            case .help:
+                break
+            case .share:
+                break
+            case .developerAccount:
+                break
+            case .contact:
+                break
+            }
+        }
     }
 
 }
