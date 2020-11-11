4576017ea4939a58259bd2bad48541e140b7d351
猫吉 <freeaddress834@gmail.com>
2020-11-11 10:18:40 +0900

    HelpDetailVC > ヘルプのどの項目かを示すenum、enumの変数を用意

--- TodaysToDo/Controller/Settings/Menu/HelpDetailViewController.swift
+++ TodaysToDo/Controller/Settings/Menu/HelpDetailViewController.swift
@@ -5,11 +5,20 @@
 //  Created by Nekokichi on 2020/11/10.
 //
 
+enum HelpType {
+    case whatIsTodaysTodo
+    case tutorialCreateTask
+    case whatIsEndTime
+    case tutorialEndTime
+    case whatIsPriority
+}
+
 import UIKit
 
 class HelpDetailViewController: UIViewController {
 
     var navigationTitle: String!
+    var helpTypeValue: HelpType!
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
