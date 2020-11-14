a740dc004744cb8984458ba55fc1235f579bc857
猫吉 <freeaddress834@gmail.com>
2020-11-14 21:09:19 +0900

    SettingsValue > UserDefaultで保存&取得を行う関数を記述

--- TodaysToDo/Controller/Settings/SettingsValue.swift
+++ TodaysToDo/Controller/Settings/SettingsValue.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class SettingsValue: NSObject, NSCoding {
 
-    private var endTimeOfTask: (x: Int?, y: Int?) = (0, 0)
-    private var numberOfTask: Int = 0
-    private var priorityOfTask: Bool = false
+    private(set) var endTimeOfTask: (x: Int, y: Int) = (0, 0)
+    private(set) var numberOfTask: Int = 0
+    private(set) var priorityOfTask: Bool = false
 
     override init() {
         super.init()
@@ -34,4 +34,31 @@ class SettingsValue: NSObject, NSCoding {
         coder.encode(priorityOfTask, forKey: "priority")
     }
 
+    // UserDetaultに保存
+    func saveSettingsValue(endTime: (Int, Int), number: Int, priority: Bool) {
+        // データ作成
+        let sv = SettingsValue()
+        sv.endTimeOfTask = endTime
+        sv.numberOfTask = number
+        sv.priorityOfTask = priority
+        // UserDefaultに保存
+        let ud = UserDefaults.standard
+        do {
+            let settingsValueData = try NSKeyedArchiver.archivedData(withRootObject: sv, requiringSecureCoding: false)
+            ud.set(settingsValueData, forKey: "settingsValueData")
+        } catch {
+            fatalError("Error of saving data in UserDefault")
+        }
+    }
+
+    // UserDefaultから取得
+    func readSettingsValue() -> SettingsValue {
+        let ud = UserDefaults.standard
+        do {
+            let settingsValueData = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(ud.object(forKey: "settingsValueData") as! Data) as! SettingsValue
+            return settingsValueData
+        } catch {
+            fatalError("Error of reading data in UserDefault")
+        }
+    }
 }
