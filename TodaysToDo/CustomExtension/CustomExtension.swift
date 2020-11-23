72455b9ef77caaa467bd3b9070eada2956c30a33
猫吉 <freeaddress834@gmail.com>
2020-11-23 22:17:37 +0900

    CustomExtension > 今月の全日をDate型で取得する関数をDate型のExtensionに追加

--- TodaysToDo/CustomExtension/CustomExtension.swift
+++ TodaysToDo/CustomExtension/CustomExtension.swift
@@ -19,6 +19,25 @@ extension Date {
         guard let sunday = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
         return gregorian.date(byAdding: .day, value: 7, to: sunday)
     }
+    var firstDayOfMonth: Date {
+        var calender = Calendar.current
+        calender.timeZone = TimeZone(identifier: "UTC")!
+        let components = calender.dateComponents([.year, .month], from: Date())
+        let startOfMonth = calender.date(from: components)
+        return startOfMonth!
+    }
+    var allDaysOfMonth: [Date] {
+        var days: [Date]! = []
+        let date = Date()
+        var calendar = Calendar.current
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+        let rangeOfThisMonth = calendar.range(of: .day, in: .month, for: date)
+        for d in 0..<rangeOfThisMonth!.last! {
+            let day = calendar.date(byAdding: .day, value: d, to: firstDayOfMonth)
+            days.append(day!)
+        }
+        return days
+    }
     func getCurrentDate() -> Date {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMdkHms", options: 0, locale: Locale(identifier: "ja_JP"))
