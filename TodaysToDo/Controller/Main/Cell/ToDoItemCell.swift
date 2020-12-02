232a3f6a2d94b6c5381ccc59b15681d3e5410589
猫吉 <freeaddress834@gmail.com>
2020-12-02 14:16:40 +0900

    ToDoItemCell > セルのドラッグ時、内部のtextFieldのテキストが他textFieldへドロップされるのを解消

--- TodaysToDo/Controller/Main/Cell/ToDoItemCell.swift
+++ TodaysToDo/Controller/Main/Cell/ToDoItemCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ToDoItemCell: UITableViewCell, UITextFieldDelegate {
+class ToDoItemCell: UITableViewCell, UITextFieldDelegate, UITextDropDelegate {
 
     // ToDoListAddVCから参照されるため、privateはなし-> Void)!
     @IBOutlet weak var todoItemTextField: UITextField!
@@ -16,6 +16,12 @@ class ToDoItemCell: UITableViewCell, UITextFieldDelegate {
     override func awakeFromNib() {
         super.awakeFromNib()
         todoItemTextField.addTarget(self, action: #selector(textFieldDidChange(sender:)), for: .editingChanged)
+        todoItemTextField.textDropDelegate = self
+    }
+
+    // textFieldをドラッグ時、中のテキストをドロップしないようにする
+    func textDroppableView(_ textDroppableView: UIView & UITextDroppable, proposalForDrop drop: UITextDropRequest) -> UITextDropProposal {
+        UITextDropProposal(operation: .cancel)
     }
 
     func resetTextField() {
