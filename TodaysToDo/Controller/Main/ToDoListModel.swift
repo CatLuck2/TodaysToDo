c0418854ca3b2407524b9117def8ed6629083dcb
猫吉 <freeaddress834@gmail.com>
2020-12-24 11:58:51 +0900

    add: ToDoListViewController用のModelとViewModelファイルを作成

---
+++ TodaysToDo/Controller/Main/ToDoListModel.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoListModel.swift
+//  TodaysToDo
+//
+//  Created by Nekokichi on 2020/12/24.
+//
+
+import Foundation
+
+class ToDoListModel {
+    
+}
---
+++ TodaysToDo/Controller/Main/ToDoListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoListViewModel.swift
+//  TodaysToDo
+//
+//  Created by Nekokichi on 2020/12/24.
+//
+
+import Foundation
+
+class ToDoListViewModel {
+    
+}
